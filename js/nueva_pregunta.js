e96c69b30ca7412e492e521fbc5067d7bbec1fc3
Gabriel Flores <gabriel.floresra@gmail.com>
2019-10-21 13:58:09 -0500

    agregado formato a las preguntas

---
+++ js/nueva_pregunta.js
@@ -0,0 +1,14 @@
+function agregaPregunta(){
+    let formulario_preguntas = document.querySelector(".formulario_preguntas");
+    let plantilla_pregunta = document.querySelector(".plantilla_pregunta");
+    let div_plantilla_pregunta = document.createElement("div");
+    div_plantilla_pregunta.innerHTML = plantilla_pregunta.innerHTML;
+
+    formulario_preguntas.appendChild(div_plantilla_pregunta);
+}
+
+function eliminarPregunta(span){
+    let plantilla_pregunta = span.parentElement.parentElement;
+    let formulario_preguntas = document.querySelector(".formulario_preguntas");
+    formulario_preguntas.removeChild(plantilla_pregunta);
+}
